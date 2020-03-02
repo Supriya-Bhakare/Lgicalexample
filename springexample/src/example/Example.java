e0f31f10b34590013b7e685da88a48012bf44a66
IPTBS <IPTBS@INBR2WKS-195436.chrysler-bundle.com>
2020-03-02 12:15:19 +0530

    example changes adding to git.

--- springexample/src/example/Example.java
+++ springexample/src/example/Example.java
@@ -5,10 +5,16 @@ public class Example {
 	public static void main(String[] args) {
 		
 		System.out.println("main method");
+		System.out.println("");
 		
 		int no=100;
-		
+		int countt=0;
+		int county=0;
 		//int i=1;
+
+		String t=" ";
+		String y=" ";
+		
 		for(int i=1;i<=100;i++)
 		{
 			int count =0;
@@ -27,15 +33,28 @@ public class Example {
 			
 			if(count ==2)
 			{
-			System.out.println("prime"+i);
+			//System.out.println("prime"+i);
+			t=t+i+ " ";
+			
+			countt++;
 			}
 			else
 			{
 				
-				System.out.println("non prime"+i);
+				//System.out.println("non prime"+i);
+				y=y+i+" ";
+				county++;
 		    }
 		}
 			
+		
+		System.out.println("prime in list");
+		System.out.println(t);
+		System.out.println("total prime: "+countt);
+		System.out.println("");
+		System.out.println("not prime no in list");
+		System.out.println(y);
+		System.out.println("Total non prime:"+county);
     }
 		
 }
